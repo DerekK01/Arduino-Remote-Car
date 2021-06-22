ae2d1354de90ecb8b8f712699d14e3a7d56427db
Derek <derekkwan01@gmail.com>
2021-06-22 16:38:59 +0800

    Update Remote Car.ino

--- Remote Car.ino
+++ Remote Car.ino
@@ -25,48 +25,48 @@ void loop()
     blueToothVal = Serial.read(); //read it
   }
   if (blueToothVal == 'c')
-  { //if value from bluetooth serial is n
+  { 
     analogWrite(motorPin3, 180);
-    analogWrite(motorPin4, 0);          //switch on LED
+    analogWrite(motorPin4, 0);          
     if (lastValue != 'c')
-      Serial.println(F("clockwise")); //print LED is on
+      Serial.println(F("clockwise")); 
     lastValue = blueToothVal;
   }
   else if (blueToothVal == 'a')
-  { //if value from bluetooth serial is n
+  { 
     analogWrite(motorPin3, 0);
-    analogWrite(motorPin4, 180);           //turn off LED
+    analogWrite(motorPin4, 180);           
     if (lastValue != 'a')
-      Serial.println(F("anti-clockwise")); //print LED is on
+      Serial.println(F("anti-clockwise")); 
     lastValue = blueToothVal;
   }
     else if (blueToothVal == 's')
-  { //if value from bluetooth serial is n
+  { 
     analogWrite(motorPin3, 0);
-    analogWrite(motorPin4, 0);           //turn off LED
+    analogWrite(motorPin4, 0);           
     if (lastValue != 's')
-      Serial.println(F("stopped")); //print LED is on
+      Serial.println(F("stopped")); 
     lastValue = blueToothVal;
   }
   else if (blueToothVal == 'm')
-  { //if value from bluetooth serial is n
-    motor.write(120);           //turn off LED
+  { 
+    motor.write(120);           
     if (lastValue != 'm')
-      Serial.println(F("small motor r")); //print LED is on
+      Serial.println(F("small motor r")); 
     lastValue = blueToothVal;
   }
     else if (blueToothVal == 'r')
-  { //if value from bluetooth serial is n
-    motor.write(60);           //turn off LED
+  { 
+    motor.write(60);           
     if (lastValue != 'r')
-      Serial.println(F("small motor l")); //print LED is on
+      Serial.println(F("small motor l")); 
     lastValue = blueToothVal;
   }
   else if (blueToothVal == 'l')
-  { //if value from bluetooth serial is n
-    motor.write(90);           //turn off LED
+  { 
+    motor.write(90);           
     if (lastValue != 'l')
-      Serial.println(F("small motor s")); //print LED is on
+      Serial.println(F("small motor s")); 
     lastValue = blueToothVal;
   }
   delay(1000);
